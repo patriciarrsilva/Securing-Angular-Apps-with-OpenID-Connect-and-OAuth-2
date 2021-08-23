bc7925d91d822d1c551f13e6bb8af8083bc3447c
Patrícia Silva <prrs89@gmail.com>
2021-08-23 08:45:04 +0100

    Configuring oidc-client to Connect to Your STS

--- src/app/core/auth-service.component.ts
+++ src/app/core/auth-service.component.ts
@@ -1,7 +1,27 @@
 import { Injectable } from '@angular/core';
+import {
+  User,
+  UserManager,
+  UserManagerSettings
+} from 'oidc-client';
+import { Constants } from '../constants';
 import { CoreModule } from './core.module';
 
 @Injectable({ providedIn: CoreModule })
 export class AuthService {
-  constructor() {}
+  private _userManager: UserManager;
+  private _user: User;
+
+  constructor() {
+    const stsSettings: UserManagerSettings = {
+      authority: Constants.stsAuthority,
+      client_id: Constants.clientId,
+      redirect_uri: `${Constants.clientRoot}signin-callback`,
+      scope: 'openid profile projects-api',
+      response_type: 'code',
+      post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`
+    };
+
+    this._userManager = new UserManager(stsSettings);
+  }
 }
