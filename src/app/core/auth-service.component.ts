8771bfb1d9c8f62f1d5d55eefb029ae02c9e815d
Patrícia Silva <prrs89@gmail.com>
2021-11-28 22:20:34 +0000

    Resolving Differences in STS Configuration

--- src/app/core/auth-service.component.ts
+++ src/app/core/auth-service.component.ts
@@ -24,7 +24,15 @@ export class AuthService {
       redirect_uri: `${Constants.clientRoot}signin-callback`,
       scope: 'openid profile projects-api',
       response_type: 'code',
-      post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`
+      // post_logout_redirect_uri: `${Constants.clientRoot}signout-callback`,
+      metadata: {
+        issuer: Constants.stsAuthority,
+        authorization_endpoint: `${Constants.stsAuthority}authorize?audience=projects-api`,
+        jwks_uri: `${Constants.stsAuthority}.well-known/jwks.json`,
+        token_endpoint: `${Constants.stsAuthority}oauth/token`,
+        userinfo_endpoint: `${Constants.stsAuthority}userinfo`,
+        end_session_endpoint: `${Constants.stsAuthority}v2/logout?client_id=${Constants.clientId}&returnTo=${Constants.clientRoot}signout-callback`
+      }
     };
 
     this._userManager = new UserManager(stsSettings);
